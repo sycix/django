15eaeb63de65b4a2051b2b04f6d53a550605627f
sycix <thestillworking@gmail.com>
2022-03-02 19:46:20 +0100

    Models field 2

--- filmyweb/admin.py
+++ filmyweb/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from.models import Film
 
-admin.site.register(Film)
+# admin.site.register(Film)
+@admin.register(Film)
+class Fil
--- filmyweb/models.py
+++ filmyweb/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Film(models.Model):
     tytul = models.CharField(max_length=64,blank=False, unique=True)
     rok = models.PositiveSmallIntegerField(default=2000)
+    opis= models.TextField(default="")
+    premiera = models.DateField(null=True, blank=True)
+    imdb_rating = models.DecimalField(max_digits=4, decimal_places=2,null=True, blank=True)
+    plakat = models.ImageField(upload_to="plakaty", null=True, blank=True)
 
     def __str__(self):
-        return self.tytul + ' (' + str(self.rok) + ')'
+        return self.tytul_z_rokiem()
 
+    def tytul_z_rokiem(self):
+        return "{} ({})".format(self.tytul,self.rok)
